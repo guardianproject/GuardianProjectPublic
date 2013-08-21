06041e5266aa3f69dd85a7847dd22f7888d2af1f
Hans-Christoph Steiner <hans@guardianproject.info>
2013-08-21 15:01:43 -0400

    + ‘Graphics/General/bizcards/PGP_QR_Codes/gen.sh’

---
+++ Graphics/General/bizcards/PGP_QR_Codes/gen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for f in `gpg --fingerprint guardianproject.info | grep 'Key fingerprint' | cut -b56-90| sed 's, ,,g'` ; do
+    KEYNAME=`gpg --fingerprint $f | grep  -A1 'Key fingerprint' | sed -n 's,uid\s*,,p'`
+    echo $KEYNAME OPENPGP4FPR:$f
+    qrencode  -o "${KEYNAME}_$f.png" -l H -i OPENPGP4FPR:$f
+done
