77cc10bd8834896e61abeeb67997db8573fcf165
Hans-Christoph Steiner <hans@guardianproject.info>
2013-08-21 15:16:04 -0400

    / ‘Graphics/General/bizcards/PGP_QR_Codes/gen.sh’

--- Graphics/General/bizcards/PGP_QR_Codes/gen.sh
+++ Graphics/General/bizcards/PGP_QR_Codes/gen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-for f in `gpg --fingerprint guardianproject.info | grep 'Key fingerprint' | cut -b56-90| sed 's, ,,g'` ; do
-    KEYNAME=`gpg --fingerprint $f | grep  -A1 'Key fingerprint' | sed -n 's,uid\s*,,p'`
-    echo $KEYNAME OPENPGP4FPR:$f
-    qrencode  -o "${KEYNAME}_$f.png" -l H -i OPENPGP4FPR:$f
+fpfile=fingerprints.txt
+
+echo " fingerprint                                       | email" > $fpfile
+echo "=======================================================================" >> $fpfile
+
+for f in `gpg --fingerprint guardianproject.info | grep 'Key fingerprint' | cut -b25-90 | sed 's, ,,g'` ; do
+    keyid=`echo $f | cut -b25-40`
+    KEYNAME=`gpg --fingerprint $f | grep  -A1 'Key fingerprint' | sed -n 's,uid\s*,,p' | sed 's, ,_,g' | sed 's,[<>],,g'`
+    echo $KEYNAME OPENPGP4FPR:$keyid
+    fingerprint=`gpg --fingerprint $f | grep 'Key fingerprint' | cut -b25-90`
+    echo "$fingerprint | $KEYNAME" >> $fpfile
+    qrencode  -o "${KEYNAME}_$keyid.png" -l H -i OPENPGP4FPR:$keyid
 done
