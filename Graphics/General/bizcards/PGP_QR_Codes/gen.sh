afa7a840488780608b022fee71a9e898b88a3a56
Hans-Christoph Steiner <hans@guardianproject.info>
2013-08-21 15:16:37 -0400

    / ‘Graphics/General/bizcards/PGP_QR_Codes/gen.sh’

--- Graphics/General/bizcards/PGP_QR_Codes/gen.sh
+++ Graphics/General/bizcards/PGP_QR_Codes/gen.sh
@@ -7,7 +7,7 @@ echo "=======================================================================" >
 
 for f in `gpg --fingerprint guardianproject.info | grep 'Key fingerprint' | cut -b25-90 | sed 's, ,,g'` ; do
     keyid=`echo $f | cut -b25-40`
-    KEYNAME=`gpg --fingerprint $f | grep  -A1 'Key fingerprint' | sed -n 's,uid\s*,,p' | sed 's, ,_,g' | sed 's,[<>],,g'`
+    KEYNAME=`gpg --fingerprint $f | grep  -A1 'Key fingerprint' | sed -n 's,uid\s*,,p' | sed 's, ,_,g' | sed 's,[<>],_,g'`
     echo $KEYNAME OPENPGP4FPR:$keyid
     fingerprint=`gpg --fingerprint $f | grep 'Key fingerprint' | cut -b25-90`
     echo "$fingerprint | $KEYNAME" >> $fpfile
